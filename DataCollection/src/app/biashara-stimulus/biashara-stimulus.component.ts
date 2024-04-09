d7b5a75873868cd1cd5d0ba917dd90f36ba8b594
Alemba-Vivian <alembavivian@gmail.com>
2024-04-09 16:44:54 +0300

    form layout structure

--- DataCollection/src/app/biashara-stimulus/biashara-stimulus.component.ts
+++ DataCollection/src/app/biashara-stimulus/biashara-stimulus.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { MatRadioModule} from '@angular/material/radio';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+
 
 @Component({
   selector: 'app-biashara-stimulus',
   standalone: true,
-  imports: [],
+  imports:[NgIf, NgClass, MatRadioModule, MatFormFieldModule, MatSelectModule, MatCheckboxModule],
   templateUrl: './biashara-stimulus.component.html',
   styleUrl: './biashara-stimulus.component.css'
 })
 export class BiasharaStimulusComponent {
+
+
+  // testing select
+  selected = 'option1';
 
 }
